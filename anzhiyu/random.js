1f68b6e09df8920df7c4098fbb60852d7b9e3113
yangjunliu <233biubiu@gmail.com>
2025-05-14 23:41:14 +0800

    Site updated: 2025-05-14 23:41:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["rainbow-bubbles/创建你的第一篇博客/","rainbow-bubbles/hello-world/"];function toRandomPost(){
+var posts=["rainbow-bubbles/Ubuntu安装php/","rainbow-bubbles/创建你的第一篇博客/","rainbow-bubbles/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
